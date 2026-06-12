2754f986fe1433674546a8322158becf062c3276
wendamys <wendamys@yandex.ru>
2026-06-12 14:59:22 +0700

    Вынес рандом в отдельный класс с методом

---
+++ src/main/java/domain/MathUtils/RandomNumber.java
@@ -0,0 +1,13 @@
+package domain.MathUtils;
+
+import java.util.Random;
+
+public class RandomNumber {
+    protected static Random random = new Random();
+
+    public int randomNumber(int from, int before) {
+        int randomNumber = random.nextInt(from, before + 1);
+        System.out.println(randomNumber);
+        return randomNumber;
+    }
+}
