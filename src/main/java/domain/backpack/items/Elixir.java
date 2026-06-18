c60b7a2d113104201241f458c0339a8591972996
leytonja <eferolgan1992@gmail.com>
2026-06-18 18:18:20 +0700

    механика рандомного занесения подтипа предмета в классы Elixir и Scroll

--- src/main/java/domain/backpack/items/Elixir.java
+++ src/main/java/domain/backpack/items/Elixir.java
@@ -1,5 +1,6 @@
 package domain.backpack.items;
 
+import domain.MathUtils.RandomNumber;
 import domain.backpack.Item;
 import domain.backpack.ItemsSubType;
 import domain.backpack.ItemsType;
@@ -7,15 +8,37 @@ import domain.navigator.Position;
 
 public class Elixir extends Item {
 
+    private ItemsSubType subType;
+
     public Elixir(String name, ItemsType type, ItemsSubType subType, int value, Position position) {
         super("E", ItemsType.ELIXIR, subType, value, position);
+        this.setSubType();
     }
+
+    /**
+     * Задает рандомный подтип предмета
+     */
+    private void setSubType() {
+        RandomNumber randomNumber = new RandomNumber();
+        switch (randomNumber.randomNumber(1, 3)) {
+            case 1 -> this.subType = ItemsSubType.HEALTH;
+            case 2 -> this.subType = ItemsSubType.AGILITY;
+            case 3 -> this.subType = ItemsSubType.STRENGTH;
+        }
+    }
+
+    @Override
+    public ItemsSubType getSubType() {
+        return subType;
+    }
+
     @Override
     public String toString() {
-        return String.format("Elixir: value %d, position(%d, %d)",
+        return String.format("Elixir: value %d, position(%d, %d), SubType %s",
                 getValue(),
                 getPosition().getX(),
-                getPosition().getY()
-                );
+                getPosition().getY(),
+                getSubType()
+        );
     }
 }
--- src/main/java/domain/backpack/items/Scroll.java
+++ src/main/java/domain/backpack/items/Scroll.java
@@ -1,20 +1,44 @@
 package domain.backpack.items;
 
+import domain.MathUtils.RandomNumber;
 import domain.backpack.Item;
 import domain.backpack.ItemsSubType;
 import domain.backpack.ItemsType;
 import domain.navigator.Position;
 
 public class Scroll extends Item {
+
+    private ItemsSubType subType;
+
     public Scroll(String name, ItemsType type, ItemsSubType subType, int value, Position position) {
         super("S", ItemsType.SCROLL, subType, value, position);
+        this.setSubType();
     }
+
+    /**
+     * Задает рандомный подтип предмета
+     */
+    private void setSubType() {
+        RandomNumber randomNumber = new RandomNumber();
+        switch (randomNumber.randomNumber(1, 3)) {
+            case 1 -> this.subType = ItemsSubType.HEALTH;
+            case 2 -> this.subType = ItemsSubType.AGILITY;
+            case 3 -> this.subType = ItemsSubType.STRENGTH;
+        }
+    }
+
+    @Override
+    public ItemsSubType getSubType() {
+        return subType;
+    }
+
     @Override
     public String toString() {
-        return String.format("Scroll: value %d, position(%d, %d)",
+        return String.format("Scroll: value %d, position(%d, %d), SubType %s",
                 getValue(),
                 getPosition().getX(),
-                getPosition().getY()
+                getPosition().getY(),
+                getSubType()
         );
     }
 }
