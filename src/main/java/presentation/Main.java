ccca710d493ccbc97e377338e461786d63685b50
wendamys <wendamys@yandex.ru>
2026-06-26 12:26:52 +0700

    fix Main

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -1,22 +1,7 @@
 package presentation;
 
-import domain.characters.Player;
-import domain.characters.enemies.Zombie;
-import domain.navigator.DirectionType;
-import domain.navigator.MovementSystem;
-import domain.navigator.Position;
-
 public class Main {
     public static void main(String[] args) {
-
-        MovementSystem mv = new MovementSystem();
-
-        Zombie zombie = new Zombie(new Position(0, 0));
-        Player player = new Player(new Position(0, 1));
-
-        DirectionType dt = zombie.convergence(player);
-        mv.moveDir(dt, zombie);
-        zombie.convergence(player);
 
     }
 }
