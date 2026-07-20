fa353cf364c19882b11e3fdc0e0a3319c11de5ea
leytonja <eferolgan1992@gmail.com>
2026-07-20 12:58:52 +0700

    GameFacade

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -22,32 +22,32 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         GameFacade facade = new GameFacadeImpl();
-//        facade.startGame(); // Uncomment to start the game
-        DataLayer.save(facade);
-        GameDTO dto = new GameDTO();
-        dto = DataLayer.loadDTO();
-
-
-        Terminal terminal = new DefaultTerminalFactory()
-                .setInitialTerminalSize(new TerminalSize(180, 80))
-                .createTerminal();
-
-        Screen screen = new TerminalScreen(terminal);
-        TextGraphics tg = screen.newTextGraphics();
-        screen.startScreen();
-
-        tg.putString(dto.getPlayerDTO().getPositionDTO().getX(),
-                dto.getPlayerDTO().getPositionDTO().getY(),
-                "@"
-        );
-        for (var item : dto.getDungeDTO().getAllItemsListDTO()) {
-            tg.putString(item.getPositionDTO().getX(), item.getPositionDTO().getY(), item.getName());
-        }
-        for (var enemy : dto.getDungeDTO().getAllEnemiesListDTO()) {
-            tg.putString(enemy.getPositionDTO().getX(), enemy.getPositionDTO().getY(), enemy.getName());
-        }
-
-        screen.refresh();
+        facade.startGame(); // Uncomment to start the game
+//        DataLayer.save(facade);
+//        GameDTO dto = new GameDTO();
+//        dto = DataLayer.loadDTO();
+//
+//
+//        Terminal terminal = new DefaultTerminalFactory()
+//                .setInitialTerminalSize(new TerminalSize(180, 80))
+//                .createTerminal();
+//
+//        Screen screen = new TerminalScreen(terminal);
+//        TextGraphics tg = screen.newTextGraphics();
+//        screen.startScreen();
+//
+//        tg.putString(dto.getPlayerDTO().getPositionDTO().getX(),
+//                dto.getPlayerDTO().getPositionDTO().getY(),
+//                "@"
+//        );
+//        for (var item : dto.getDungeDTO().getAllItemsListDTO()) {
+//            tg.putString(item.getPositionDTO().getX(), item.getPositionDTO().getY(), item.getName());
+//        }
+//        for (var enemy : dto.getDungeDTO().getAllEnemiesListDTO()) {
+//            tg.putString(enemy.getPositionDTO().getX(), enemy.getPositionDTO().getY(), enemy.getName());
+//        }
+//
+//        screen.refresh();
 
     }
 }
