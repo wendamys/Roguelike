333ea173119a4833772e9ae24625a3731b0217b8
wendamys <wendamys@yandex.ru>
2026-07-21 23:07:43 +0700

    Controller ведёт весь цикл приложения (меню/игра/конец); Main создаёт общий Screen

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -1,11 +1,24 @@
 package presentation;
 
-import domain.gameSession.Game;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new Controller(new Game(), new UIView()).run();
+        Terminal terminal = new DefaultTerminalFactory()
+                .setInitialTerminalSize(new TerminalSize(120, 62))
+                .createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+
+        StartScreen startScreen = new StartScreen(screen);
+        UIView uiView = new UIView(screen);
+        EndScreen endScreen = new EndScreen(screen);
+
+        new Controller(startScreen, uiView, endScreen).run();
     }
 }
